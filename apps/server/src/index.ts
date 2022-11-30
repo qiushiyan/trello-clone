5587ee2d3c5ac022e06b466afffca993e5c303f9
Qiushi Yan <qiushi.yann@gmail.com>
2022-11-30 15:40:10 -0600

    basic server setup

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,37 @@
-import { hello } from "./greetings";
+import express from "express";
+import { createServer } from "http";
 import { Server } from "socket.io";
+import bodyParser from "body-parser";
+import * as userController from "./controllers/user.controller";
+import { startDb } from "./db";
+import dotenv from "dotenv";
+import authMiddleware from "./middlewares/auth";
 
-console.log(hello());
+dotenv.config();
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.post("/api/users/new", userController.register);
+app.post("/api/users/login", userController.login);
+app.use("/api/users/me", authMiddleware, userController.currentUser);
+
+io.on("connection", (socket) => {
+  console.log(`websocket connected ${socket.id}`);
+});
+
+const bootstrap = async () => {
+  await startDb();
+  httpServer.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port} 🚀`);
+  });
+};
+
+bootstrap();
